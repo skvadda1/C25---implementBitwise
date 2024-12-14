bb78fa5f9dd183c863e922974c781526f79c28cb
skvadda1 <156102921+skvadda1@users.noreply.github.com>
2024-12-14 10:05:49 -0500

    Add files via upload

---
+++ implementBitwise.py
@@ -0,0 +1,10 @@
+num1 = 10
+num2 = 4
+
+print("Num 1 & Num 2 are:" , num1,num2)
+print("Num 1 | Num 2 are:" , num1 | num2)
+print("Num 1 ^ Num 2 are:" , num1 ^ num2)
+print("Num 1 << Num 2 are:" , num1 << num2)
+print("Num 1 >> Num 2 are:" , num1 >> num2)
+print("~Num1 a is:" , ~num1)
+print("~Num2 a is:" , ~num2)
